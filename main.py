97959483948de65f896de0137f1baf6439696996
Javier Nunez <jnune006@fiu.edu>
2022-07-17 02:40:34 -0400

    Intro fast api

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-if __name__ == '__main__':
-    from classes.fantasypros import FantasyPros
-    import time
-    from classes.config import Config
-    from classes.yahoo import Yahoo
-    from classes.datamerge import DataMerge
-    from classes.playerfactory import PlayerFactory
+from fastapi import FastAPI
+from classes.fantasypros import FantasyPros
+import time
+from classes.config import Config
+from classes.yahoo import Yahoo
+from classes.datamerge import DataMerge
+from classes.playerfactory import PlayerFactory
 
+    
+    
+app = FastAPI()
+@app.get("/")
+def get_data():
     start = time.perf_counter()
     config = Config.get_config()
     fpros_data = FantasyPros.get_fpros_data(config)
@@ -13,5 +18,5 @@ if __name__ == '__main__':
     merged_data_result = DataMerge.merge_data(fpros_data, yahoo_data)
     players_dict = PlayerFactory.create_players(merged_data_result)
     end = time.perf_counter()
-    
     print(f'Finished in {round(end-start, 2)} second(s)')
+    return players_dict
